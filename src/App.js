6d5cb038329de8c3d71bce22e6b8a8a76b2ca2ac
Rakshitkulkarni223 <rakshitkulkarni2002@gmail.com>
2025-11-10 16:02:02 +0530

    Inital working code.

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
     setCurrentIndex(0);
 
     try {
-      const response = await fetch("http://localhost:4000/generate", {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/generate`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ topic }),
